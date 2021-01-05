b2e0a3c4e77cc14b341a03aca0ee08a37628572f
mj2720 <72764338+mj2720@users.noreply.github.com>
2021-01-05 10:33:42 -0500

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 while True: 
      print("Light level:" + input.light_level()) 
      if input.light_level() > 20: 
-         light.show_animation(light.rainbowAnimation, 5000) 
+         light.set_all(light.rgb(255,255,255))
      else: light.clear()
--- main.ts
+++ main.ts
@@ -1,7 +1,7 @@
 while (true) {
     console.log("Light level:" + input.lightLevel())
     if (input.lightLevel() > 20) {
-        light.showAnimation(light.rainbowAnimation, 5000)
+        light.setAll(light.rgb(255, 255, 255))
     } else {
         light.clear()
     }
