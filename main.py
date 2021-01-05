bd905eb43d3bd4eb9201377db1258c969bbad7a9
mj2720 <72764338+mj2720@users.noreply.github.com>
2021-01-05 16:11:37 -0500

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 while True: 
      print("Light level:" + input.light_level()) 
      if input.light_level() > 20: 
-         light.set_all(light.rgb(255,165,0))
+         light.set_all(light.rgb(255,215,0))
      else: light.clear()
